2ee8e83b4d8a3af201473a7d43b284788b0c1fe4
xiaojiaqi406 <38806396+xiaojiaqi406@users.noreply.github.com>
2020-01-29 21:51:12 +0800

    add file python

---
+++ demo.py
@@ -0,0 +1,171 @@
+#生成斐波那契数列的前20个数。
+
+def fib(n):
+    if n==1:
+        return 1
+    elif n==2:
+        return 2
+    else:
+        return  fib(n-1)+fib(n-2)
+
+# for i in range(1,20):
+#     print(fib(i))
+#
+
+#找出10000以内的完美数。
+
+for i in range(2,10000):
+    s = 0
+    for j in range(1,i):
+        if i%j==0:
+            s+=j
+    if s==i:
+        print(s)
+
+#输出100以内所有的素数。
+import math
+num=[2,3]
+for i in range(5,100):
+    flag=True
+    for j in range(2,math.sqrt(i)+1):
+        if i%j==0:
+            flag=False
+            break
+    if flag:
+        num.append(i)
+
+
+
+#设计一个函数产生指定长度的验证码，验证码由大小写字母和数字构成。
+def generate_code(code_len=4):
+
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars) - 1
+    code = ''
+    for _ in range(code_len):
+        index = random.randint(0, last_pos)
+        code += all_chars[index]
+    return code
+
+#设计一个函数返回传入的列表中最大和第二大的元素的值。
+def max2(x):
+    m1, m2 = (x[0], x[1]) if x[0] > x[1] else (x[1], x[0])
+    for index in range(2, len(x)):
+        if x[index] > m1:
+            m2 = m1
+            m1 = x[index]
+        elif x[index] > m2:
+            m2 = x[index]
+    return m1, m2
+
+
+进程：进程就是操作系统中执行的一个程序，操作系统以进程为单位分配存储空间，每个进程都有自己的地址空间、数据栈以及其他用于跟踪进程执行的辅助数据，操作系统管理所有进程的执行，为它们合理的分配资源
+from random import randint
+from threading import Thread
+from time import time, sleep
+
+
+def download(filename):
+    print('开始下载%s...' % filename)
+    time_to_download = randint(5, 10)
+    sleep(time_to_download)
+    print('%s下载完成! 耗费了%d秒' % (filename, time_to_download))
+
+
+def main():
+    start = time()
+    t1 = Thread(target=download, args=('Python从入门到住院.pdf',))
+    t1.start()
+    t2 = Thread(target=download, args=('Peking Hot.avi',))
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('总共耗费了%.3f秒' % (end - start))
+
+
+if __name__ == '__main__':
+    main()
+
+线程：一个进程还可以拥有多个并发的执行线索，简单的说就是拥有多个可以获得CPU调度的执行单元，这就是所谓的线程。由于线程在同一个进程下，它们可以共享相同的上下文，因此相对于进程而言，线程间的信息共享和通信更加容易。
+from random import randint
+from threading import Thread
+from time import time, sleep
+
+
+def download(filename):
+    print('开始下载%s...' % filename)
+    time_to_download = randint(5, 10)
+    sleep(time_to_download)
+    print('%s下载完成! 耗费了%d秒' % (filename, time_to_download))
+
+
+def main():
+    start = time()
+    t1 = Thread(target=download, args=('Python从入门到住院.pdf',))
+    t1.start()
+    t2 = Thread(target=download, args=('Peking Hot.avi',))
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('总共耗费了%.3f秒' % (end - start))
+
+
+if __name__ == '__main__':
+    main()
+
+# 使用多进程对复杂任务进行“分而治之”。
+# 我们来完成1~100000000求和的计算密集型任务，
+from time import time
+
+
+def main():
+    total = 0
+    number_list = [x for x in range(1, 100000001)]
+    start = time()
+    for number in number_list:
+        total += number
+    print(total)
+    end = time()
+    print('Execution time: %.3fs' % (end - start))
+
+
+from multiprocessing import Process, Queue
+from random import randint
+from time import time
+
+
+def task_handler(curr_list, result_queue):
+    total = 0
+    for number in curr_list:
+        total += number
+    result_queue.put(total)
+
+
+def main():
+    processes = []
+    number_list = [x for x in range(1, 100000001)]
+    result_queue = Queue()
+    index = 0
+    for _ in range(8):
+        p = Process(target=task_handler,
+                    args=(number_list[index:index + 12500000], result_queue))
+        index += 12500000
+        processes.append(p)
+        p.start()
+
+    start = time()
+    for p in processes:
+        p.join()
+
+    total = 0
+    while not result_queue.empty():
+        total += result_queue.get()
+    print(total)
+    end = time()
+    print('Execution time: ', (end - start), 's', sep='')
+
+
+if __name__ == '__main__':
+    main()
